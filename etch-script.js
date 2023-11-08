0959cd427bd4318f256714d22770ead4ef3328ba
Kajamugesh Raneethran <mugesh.raneethran@mail.utoronto.ca>
2023-11-08 17:52:38 -0500

    add comments

--- etch-script.js
+++ etch-script.js
@@ -1,18 +1,27 @@
 const grid = document.querySelector('.grid');
 
 function setUpGrid(width, height){
+
+  // Total number of squares
   squares = width * height;
 
+
+  // 750px are the base dimensions of the grid
   ratio = width/height;
   gridWidth = 750 * ratio;
   gridHeight = 750 / ratio;
+
+  // Set width and height of grid in terms of pixels
   grid.setAttribute('style', `width: ${gridWidth}px; height: ${gridHeight}px;`);
 
 
-  console.log(squares);
+  // Add every grid square to grid
   for (let i = 0; i < squares; i++) {
+
     const square = document.createElement('div');
     square.classList.add('grid-square');
+
+    // Dimensions of grid square depend on the lowest value dimension of the grid
     if(width >= height){
       square.setAttribute('style', `height: ${gridWidth/width}px; width: ${gridWidth/width}px;`);
     }
@@ -23,4 +32,5 @@ function setUpGrid(width, height){
     grid.appendChild(square);
   }
 }
+
 setUpGrid(16,16);
